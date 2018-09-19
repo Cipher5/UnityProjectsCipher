52d1387a1d6302a01d3799f8430ccf4f8c5e7533
mvcodeclub <sarah@mvcodeclub.com>
2018-09-18 18:34:06 -0700

    I added two hedgehogs, started a movement script

---
+++ SuperRollABall/Assets/Scripts/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	Rigidbody rb;
+	public float moveSpeed = 40f;
+	// Use this for initialization
+	void Start () {
+
+		rb = this.GetComponent<Rigidbody>();
+	}
+	
+	//  once per frame
+	void Update () {
+
+		rb.AddForce(forceVector);
+		float hdir = Input.GetAxisRaw ("Horizontal");
+		float vdir = Input.GetAxisRaw ("Vertical");
+
+		Vector3 directionVector = new Vector3 (hdir, 0, vdir);
+		Vector3 unitVector = directionVecter.normalized;
+		Vector3 forceVector = unitVector * moveSpeed * Time.deltaTime;
+	}
+}
