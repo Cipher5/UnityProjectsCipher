9e742e7c5f4f3fe2433c0a61f9c4321fc47a1cca
mvcodeclub <sarah@mvcodeclub.com>
2018-10-13 15:02:33 -0700

    Added a castle, another mega kaiju, finished animation.

--- SuperRollABall/Assets/Scripts/collectiblesController.cs
+++ SuperRollABall/Assets/Scripts/collectiblesController.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class collectiblesController : MonoBehaviour {
 	
 	public bool isCollected;
+	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
 		
 		isCollected = false;
-
+		rb = this.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isCollected) 
 		{
-			Debug.Log ("Splut");
-			Destroy (this.gameObject);
+			Destroy(this.gameObject, 1.5f);
+			Disappear();
+
 		}
+	}
+
+	void Disappear() {
+		this.transform.Rotate(Vector3.up, 720 * Time.deltaTime);
+		this.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+		this.transform.localScale += new Vector3(.25f, .25f, .25f) * Time.deltaTime;
 	}
 }
